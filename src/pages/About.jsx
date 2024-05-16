3db5237c392a5ab0a26594ca4a52cee8ddf39421
FixFire <89625074+NatthawutSK@users.noreply.github.com>
2024-05-16 16:30:56 +0700

    test PR

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ function About() {
   return (
     <div>
       <h2>About</h2>
-      <p>This is the About page. 5555</p>
+      <p>This is the About page. 5555 PRPRPR</p>
       <a href="/">Home</a>
     </div>
   );
